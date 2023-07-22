9c6e5532104ebbcd3d99e384cb4a7d0bd418b306
AkbarNurDzikri <dzikrinurakbar94@gmail.com>
2023-07-22 13:40:04 +0700

    create request-http.test file

---
+++ test/request-http.test.js
@@ -0,0 +1,15 @@
+import express, { query } from 'express';
+import request from 'supertest';
+
+const app = express();
+
+// untuk menjalankan unit test menggunakan supertest package tidak perlu menjalankan server
+
+app.get('/', (req, res) => {
+  res.send(`Hello, ${req.query.name}`);
+});
+
+test('Test http get method with params', async () => {
+  const response = await request(app).get('/').query({name: 'Dzikri Nur Akbar'});
+  expect(response.text).toBe('Hello, Dzikri Nur Akbar');
+});
