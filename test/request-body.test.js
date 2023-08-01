c3a0143f1d2e02f25a15024f7c2e7b6ea7bf2519
AkbarNurDzikri <dzikrinurakbar94@gmail.com>
2023-08-01 15:52:24 +0700

    try upload file with mustache express

--- test/request-body.test.js
+++ test/request-body.test.js
@@ -1,9 +1,11 @@
-import express from 'express';
+import express, { text } from 'express';
 import request from 'supertest';
+import expressFileUpload from 'express-fileupload';
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(expressFileUpload());
 
 app.post('/json', (req, res) => {
   const name = req.body.name;
@@ -17,6 +19,13 @@ app.post('/form', (req, res) => {
   res.json({
     hello: `Hello ${name}`,
   });
+});
+
+app.post('/file', async (req, res) => {
+  const textFile = req.files.article;
+  await textFile.mv(__dirname + '/upload/' + textFile.name);
+
+  res.send(`Hello ${req.body.name}, you uploaded ${textFile.name}`);
 });
 
 test('Test body form (application/json)', async () => {
@@ -37,4 +46,13 @@ test('Test body form (application/x-www-form-urlencoded)', async () => {
     expect(response.body).toEqual({
       hello: 'Hello World',
     });
+});
+
+test('Test request File Upload', async () => {
+  const response = await request(app).post('/file')
+    .set('content-type', 'multipart/form-data')
+    .field('name', 'Dzikri')
+    .attach('article', __dirname + '/contoh.txt');
+
+    expect(response.text).toBe('Hello Dzikri, you uploaded contoh.txt')
 });
